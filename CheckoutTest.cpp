d81e257c279ad43a55a3b263495f3cbda4d92b8a
mange1990 <renen90@hotmail.com>
2020-01-29 14:46:10 +0100

    Update CheckoutTest.cpp

--- CheckoutTest.cpp
+++ CheckoutTest.cpp
@@ -5,7 +5,7 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+//Tester
 #include <iostream>
 #include <gtest/gtest.h>
 #include "Checkout.h"
